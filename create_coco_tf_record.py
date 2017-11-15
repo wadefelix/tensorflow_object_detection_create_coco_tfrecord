520769248828a57e807bf013816ac655395e9b2a
renwei <renwei@mergesecrity.com>
2017-11-15 10:42:20 +0800

    writing data while reading them, if memory not enough

--- create_coco_tf_record.py
+++ create_coco_tf_record.py
@@ -40,14 +40,15 @@ flags.DEFINE_bool('shuffle_imgs',True,'whether to shuffle images of coco')
 FLAGS = flags.FLAGS
 
 
-def load_coco_dection_dataset(imgs_dir, annotations_filepath, shuffle_img = True ):
+def convert_coco_dection_dataset(imgs_dir, annotations_filepath, output_filepath, shuffle_img = True ):
     """Load data from dataset by pycocotools. This tools can be download from "http://mscoco.org/dataset/#download"
     Args:
         imgs_dir: directories of coco images
         annotations_filepath: file path of coco annotations file
+        output_filepath: file path of tfrecord file
         shuffle_img: wheter to shuffle images order
     Return:
-        coco_data: list of dictionary format information of each image
+        None
     """
     coco = COCO(annotations_filepath)
     img_ids = coco.getImgIds() # totally 82783 images
@@ -57,42 +58,42 @@ def load_coco_dection_dataset(imgs_dir, annotations_filepath, shuffle_img = True
     if shuffle_img:
         shuffle(img_ids)
 
-    coco_data = []
-
     nb_imgs = len(img_ids)
-    for index, img_id in enumerate(img_ids):
-        if index % 100 == 0:
-            print("Readling images: %d / %d "%(index, nb_imgs))
-        img_info = {}
-        bboxes = []
-        labels = []
+    with tf.python_io.TFRecordWriter(output_filepath) as tfrecord_writer:
+        for index, img_id in enumerate(img_ids):
+            if index % 100 == 0:
+                print("Converting images: %d / %d" % (index, nb_imgs))
+            img_info = {}
+            bboxes = []
+            labels = []
 
-        img_detail = coco.loadImgs(img_id)[0]
-        pic_height = img_detail['height']
-        pic_width = img_detail['width']
+            img_detail = coco.loadImgs(img_id)[0]
+            pic_height = img_detail['height']
+            pic_width = img_detail['width']
 
-        ann_ids = coco.getAnnIds(imgIds=img_id,catIds=cat_ids)
-        anns = coco.loadAnns(ann_ids)
-        for ann in anns:
-            bboxes_data = ann['bbox']
-            bboxes_data = [bboxes_data[0]/float(pic_width), bboxes_data[1]/float(pic_height),\
-                                  bboxes_data[2]/float(pic_width), bboxes_data[3]/float(pic_height)]
-                         # the format of coco bounding boxs is [Xmin, Ymin, width, height]
-            bboxes.append(bboxes_data)
-            labels.append(ann['category_id'])
+            ann_ids = coco.getAnnIds(imgIds=img_id,catIds=cat_ids)
+            anns = coco.loadAnns(ann_ids)
+            for ann in anns:
+                bboxes_data = ann['bbox']
+                bboxes_data = [bboxes_data[0]/float(pic_width), bboxes_data[1]/float(pic_height),\
+                                      bboxes_data[2]/float(pic_width), bboxes_data[3]/float(pic_height)]
+                             # the format of coco bounding boxs is [Xmin, Ymin, width, height]
+                bboxes.append(bboxes_data)
+                labels.append(ann['category_id'])
 
+            img_path = os.path.join(imgs_dir, img_detail['file_name'])
+            img_bytes = tf.gfile.FastGFile(img_path,'rb').read()
 
-        img_path = os.path.join(imgs_dir, img_detail['file_name'])
-        img_bytes = tf.gfile.FastGFile(img_path,'rb').read()
+            img_info['pixel_data'] = img_bytes
+            img_info['height'] = pic_height
+            img_info['width'] = pic_width
+            img_info['bboxes'] = bboxes
+            img_info['labels'] = labels
 
-        img_info['pixel_data'] = img_bytes
-        img_info['height'] = pic_height
-        img_info['width'] = pic_width
-        img_info['bboxes'] = bboxes
-        img_info['labels'] = labels
-
-        coco_data.append(img_info)
-    return coco_data
+            example = dict_to_coco_example(img_info)
+            tfrecord_writer.write(example.SerializeToString())
+        print("Converting is done for %d images." % (nb_imgs,))
+    return None
 
 
 def dict_to_coco_example(img_data):
@@ -124,6 +125,7 @@ def dict_to_coco_example(img_data):
     }))
     return example
 
+
 def main(_):
     if FLAGS.set == "train":
         imgs_dir = os.path.join(FLAGS.data_dir, 'train2014')
@@ -135,17 +137,8 @@ def main(_):
         print("Convert coco val file to tf record")
     else:
         raise ValueError("you must either convert train data or val data")
-    # load total coco data
-    coco_data = load_coco_dection_dataset(imgs_dir,annotations_filepath,shuffle_img=FLAGS.shuffle_imgs)
-    total_imgs = len(coco_data)
-    # write coco data to tf record
-    with tf.python_io.TFRecordWriter(FLAGS.output_filepath) as tfrecord_writer:
-        for index, img_data in enumerate(coco_data):
-            if index % 100 == 0:
-                print("Converting images: %d / %d" % (index, total_imgs))
-            example = dict_to_coco_example(img_data)
-            tfrecord_writer.write(example.SerializeToString())
-
+    # converting total coco data
+    convert_coco_dection_dataset(imgs_dir, annotations_filepath, FLAGS.output_filepath, shuffle_img=FLAGS.shuffle_imgs)
 
 if __name__ == "__main__":
     tf.app.run()
